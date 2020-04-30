5de8c7e04c87aaef9ce6165c8c1aab920efb356d
Francesca Palumbo <econofra87@gmail.com>
2020-04-30 09:12:04 +0200

    da capirci qualcosa

---
+++ main.js
@@ -0,0 +1,12 @@
+$(".hamburger-menu").click(function);
+
+$("ul.hamburger-menu li").show(2000);
+
+$("ul.hamburger-menu li a").show(2000);
+
+
+$(".hamburger-menu li").show(2000);
+
+$("ul.hamburger-menu li:hidden").show(2000);
+
+$("ul.hamburger-menu has (ul)").show(2000);
